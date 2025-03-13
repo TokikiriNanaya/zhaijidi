a2e570b9f603ecd1ce18329a02f7d5f95fd8dbd2
zhangpw <1406497296@qq.com>
2025-03-13 14:36:39 +0800

    测试运行

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # 300英雄宅基地自动完成每日任务
 
 token填写到config.yaml中
+
+## 获取token的方法
+
+1. 手机抓包（推荐，但或许需要root权限）
+   这里使用了HttpCanary，点击左上角选择目标应用中选择宅基地，右下角开启抓包后打开宅基地即可抓包
+
+   ![image.png](assets/HttpCanary01.png)
+
+   随便选一个包，选择请求，预览，找到token（没有token就换个请求）
+
+   ![36ed38cb024d2c3d22b63a10248170a8_720.jpg](assets/HttpCanary02.png)
+
+2. PC浏览器抓包（会与手机宅基地相互挤号，因此不推荐，作为方法1无法使用时的下位）
+   进入http://300zjdclient.tygms.cn/，使用手机验证码登录
+   ![img.png](assets/img.png)
+   然后按f12，依次点击“应用”，“本地存储空间”，找到ClientToken，复制value（注意，不需要双引号）
+   ![img_1.png](assets/img_1.png)
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2025/3/13 12:45
-# @Author  : Kiriya
-# @File    : main.py
-# @Description : 宅基地日常任务
-
 import requests
 import yaml
+import ssl
+import time
+import json  # 导入 json 模块用于解析 JSON 字符串
+from requests.adapters import HTTPAdapter
+
+
+# 自定义适配器以使用 pyOpenSSL
+class PyOpenSSLAdapter(HTTPAdapter):
+    def init_poolmanager(self, *args, **kwargs):
+        context = ssl.create_default_context()
+        context.load_default_certs()
+        kwargs['ssl_context'] = context
+        return super(PyOpenSSLAdapter, self).init_poolmanager(*args, **kwargs)
 
 
 # 读取 YAML 配置文件
@@ -24,12 +30,12 @@ def get_headers():
         'Cache-Control': 'no-cache',
         'Connection': 'keep-alive',
         'Content-Type': 'application/json',
-        'Origin': 'https://300zjdclient.tygms.cn',
+        'Origin': 'http://300zjdclient.tygms.cn',
         'Pragma': 'no-cache',
-        'Referer': 'https://300zjdclient.tygms.cn/',
+        'Referer': 'http://300zjdclient.tygms.cn/',
         'Sec-Fetch-Dest': 'empty',
         'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-site',
+        'Sec-Fetch-Site': 'cross-site',
         'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Mobile Safari/537.36',
         'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
         'sec-ch-ua-mobile': '?1',
@@ -37,30 +43,49 @@ def get_headers():
     }
 
 
-# 点赞请求
-def like_post(token, msgid, unique_id, posts_id, like_type):
+# 创建会话并使用自定义适配器
+session = requests.Session()
+session.mount('https://', PyOpenSSLAdapter())
+
+
+# 校验 token 请求
+def validate_token(token):
     url = "https://300zjd.tygms.cn/"
     headers = get_headers()
 
     data = {
-        "msgid": msgid,
+        "msgid": 1004,
+        "token": token
+    }
+
+    response = session.post(url, headers=headers, json=data)
+    return response.json()  # 返回 JSON 格式的响应
+
+
+# 点赞请求
+def like_post(token, unique_id, posts_id, like_type):
+    url = "https://300zjd.tygms.cn/"
+    headers = get_headers()
+
+    data = {
+        "msgid": 1018,
         "unique_id": unique_id,
         "posts_id": posts_id,
         "like_type": like_type,
         "token": token
     }
 
-    response = requests.post(url, headers=headers, json=data)
+    response = session.post(url, headers=headers, json=data)
     return response.text
 
 
 # 查询战绩请求
-def query_performance(token, msgid, account_id, guid, role_name, role_id):
+def query_performance(token, unique_id, account_id, guid, role_name, role_id):
     url = "https://300zjd.tygms.cn/"
     headers = get_headers()
 
     data = {
-        "msgid": msgid,
+        "msgid": 1009,
         "AccountID": account_id,
         "Guid": guid,
         "RoleName": role_name,
@@ -68,56 +93,72 @@ def query_performance(token, msgid, account_id, guid, role_name, role_id):
         "token": token
     }
 
-    response = requests.post(url, headers=headers, json=data)
+    response = session.post(url, headers=headers, json=data)
     return response.text
 
 
 # 查询历史战绩请求
-def query_history(token, msgid, role_id, match_type, search_index):
+def query_history(token, unique_id, role_id, match_type, search_index):
     url = "https://300zjd.tygms.cn/"
     headers = get_headers()
 
     data = {
-        "msgid": msgid,
+        "msgid": 1011,
         "RoleID": role_id,
         "MatchType": match_type,
         "SearchIndex": search_index,
         "token": token
     }
 
-    response = requests.post(url, headers=headers, json=data)
+    response = session.post(url, headers=headers, json=data)
     return response.text
 
 
 # 关注请求
-def follow_user(token, msgid, unique_id, follow_id, follow_type):
+def follow_user(token, unique_id, follow_id, follow_type):
     url = "https://300zjd.tygms.cn/"
     headers = get_headers()
 
     data = {
-        "msgid": msgid,
+        "msgid": 1029,
         "unique_id": unique_id,
         "follow_id": follow_id,
         "follow_type": follow_type,
         "token": token
     }
 
-    response = requests.post(url, headers=headers, json=data)
+    response = session.post(url, headers=headers, json=data)
     return response.text
 
 
 # 签到请求
-def sign_in(token, msgid, unique_id):
+def sign_in(token, unique_id):
     url = "https://300zjd.tygms.cn/"
     headers = get_headers()
 
     data = {
-        "msgid": msgid,
+        "msgid": 1072,
         "unique_id": unique_id,
         "token": token
     }
 
-    response = requests.post(url, headers=headers, json=data)
+    response = session.post(url, headers=headers, json=data)
+    return response.text
+
+
+# 一键领取请求
+def claim_rewards(token, unique_id, task_id_list):
+    url = "https://300zjd.tygms.cn/"
+    headers = get_headers()
+
+    data = {
+        "msgid": 1074,
+        "unique_id": unique_id,
+        "task_id_list": task_id_list,
+        "token": token
+    }
+
+    response = session.post(url, headers=headers, json=data)
     return response.text
 
 
@@ -126,27 +167,54 @@ if __name__ == "__main__":
     config = load_config('config.yaml')
     token = config['token']
 
-    # 点赞
-    like_response = like_post(token, 1018, "p492304087396782080", 6697, 1)
-    print("点赞响应:", like_response)
-    # 取消点赞
-    like_response = like_post(token, 1018, "p492304087396782080", 6697, 2)
-    print("点赞响应:", like_response)
+    # 校验 token
+    validation_response = validate_token(token)
+    if not validation_response.get("MSG"):  # 检查 MSG 字段是否为空
+        print("Token 无效，请检查您的 token。")
+    else:
+        # 解析 MSG 字段
+        msg_data = json.loads(validation_response["MSG"])
+        unique_id = msg_data["UniqueId"]  # 保持为字符串
+        account_id = msg_data["JumpwUID"]
+        guid = msg_data["JumpwGuid"]
+        role_id = int(msg_data["JumpwRoleId"])  # 转换为整数
+        role_name = msg_data["JumpwRoleName"]
 
-    # 查询战绩
-    performance_response = query_performance(token, 1009, 223164563, 20031, "", 0)
-    print("查询战绩响应:", performance_response)
-    # 查询历史战绩
-    history_response = query_history(token, 1011, 2419290158, 1, 1)
-    print("查询历史战绩响应:", history_response)
+        # 点赞
+        like_response = like_post(token, unique_id, 6697, 1)
+        print("点赞响应:", like_response)
+        time.sleep(1)  # 延迟1秒
 
-    # 关注
-    follow_response = follow_user(token, 1029, "p492304087396782080", "p492210972677771264", 1)
-    print("关注响应:", follow_response)
-    # 取消关注
-    follow_response = follow_user(token, 1029, "p492304087396782080", "p492210972677771264", 2)
-    print("关注响应:", follow_response)
+        # 取消点赞
+        like_response = like_post(token, unique_id, 6697, 2)
+        print("取消点赞响应:", like_response)
+        time.sleep(1)  # 延迟1秒
 
-    # 签到示例
-    sign_in_response = sign_in(token, 1072, "p492304087396782080")
-    print("签到响应:", sign_in_response)
+        # 查询战绩
+        performance_response = query_performance(token, unique_id, account_id, guid, "", role_id)
+        print("查询战绩响应:", performance_response)
+        time.sleep(1)  # 延迟1秒
+
+        # 查询历史战绩
+        history_response = query_history(token, unique_id, role_id, 1, 1)
+        print("查询历史战绩响应:", history_response)
+        time.sleep(1)  # 延迟1秒
+
+        # 关注
+        follow_response = follow_user(token, unique_id, "p492210972677771264", 1)
+        print("关注响应:", follow_response)
+        time.sleep(1)  # 延迟1秒
+
+        # 取消关注
+        follow_response = follow_user(token, unique_id, "p492210972677771264", 2)
+        print("取消关注响应:", follow_response)
+        time.sleep(1)  # 延迟1秒
+
+        # 签到示例
+        sign_in_response = sign_in(token, unique_id)
+        print("签到响应:", sign_in_response)
+        time.sleep(1)  # 延迟1秒
+
+        # 一键领取示例
+        claim_response = claim_rewards(token, unique_id, "1,2,3,4,5,6,7,8,9")
+        print("一键领取响应:", claim_response)
